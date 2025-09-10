05c2d182f0507385ae9bc1c192e87e7a560f17c2
niiko <nicolas.rocagel@gmail.com>
2025-09-10 18:55:32 +0200

    🚧 Testing

---
+++ src/components/Test.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+export const Test = () => {
+    const [count, setCount] = useState(0)
+
+    const handleClick = () => {
+        setCount(count + 1)
+    }
+
+    return (
+        <div>
+            <h1>Hello World</h1>
+            <button onClick={handleClick}>Click me</button>
+            <p>Count: {count}</p>
+        </div>
+    )
+}
